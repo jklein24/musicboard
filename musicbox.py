195cd4861ce3c2f7276dad45aeffb8ff0f5152a2
Jeremy Klein <jklein24@gmail.com>
2016-08-13 03:01:34 +0000

    LEDs work

--- musicbox.py
+++ musicbox.py
@@ -2,8 +2,11 @@
 
 import time
 import os
+import threading
 import pygame
 import pygame.mixer
+from gpiozero import LED
+from time import sleep
 from pygame.locals import *
 from pygame.mixer import Sound
 
@@ -23,6 +26,15 @@ KEY_TO_SOUND = {
   K_v: 'hi_hat_closed'
 }
 
+SOUND_TO_LED = {
+  'cymbal': LED(17),
+  'snare': LED(18),
+  'low_tom': LED(27),
+  'high_tom': LED(22),
+  'kick': LED(23),
+  'hi_hat_closed': LED(24)
+}
+
 def log(message, *args):
   if __debug__:
     print(message.format(*args))
@@ -38,11 +50,43 @@ def build_kit(filenames, prefix=''):
     'hi_hat_closed': Sound('samples/' + prefix + filenames[5] + '.wav')
   }
 
+class LedThread (threading.Thread):
+  threadLock = threading.Lock()
+  threads = []
+
+  def __init__(self, led):
+    threading.Thread.__init__(self)
+    self.threadId = led.pin.number
+    self.led = led
+
+  def run(self):
+    try:
+      LedThread.threadLock.acquire()
+      if self.threadId in LedThread.threads:
+        LedThread.threadLock.release()
+        return
+      LedThread.threads.append(self.threadId)
+      LedThread.threadLock.release()
+      log("LED #{0} on", self.threadId)
+      self.led.on()
+      sleep(1)
+      self.led.off()
+      log("LED #{0} off", self.threadId)
+      LedThread.threads.remove(self.threadId)
+    except Exception as e:
+      print "Exiting %s due to Error: %s"%(self.name,str(e))
+
+def flash_led(led):
+  thread = LedThread(led)
+  thread.daemon = True
+  thread.start()
+
 def handle_key(key):
   global kit_index
   if key in KEY_TO_SOUND.keys():
     log('pressed {0}', KEY_TO_SOUND[key])
     kits[kit_index][KEY_TO_SOUND[key]].play()
+    flash_led(SOUND_TO_LED[KEY_TO_SOUND[key]])
   elif key == K_z:
     kit_index = (kit_index + 1) % len(kits)
     log('new kit_index: {0}', kit_index)
